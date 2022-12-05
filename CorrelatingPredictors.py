9decd5866e1b7b5aed6dbd38ef9379b04b9dab1a
AndrewWangSCU <78170978+AndrewWangSCU@users.noreply.github.com>
2022-12-04 18:28:42 -0800

    Added GShare Implementation and data
    Modified CorrelatingPredictors.py to add GShare support and added reports as a new csv file (could have been added to existing csv file)

--- CorrelatingPredictors.py
+++ CorrelatingPredictors.py
@@ -3,6 +3,7 @@ import math
 
 # Correlating predictor (global) (5,1) predictor with 5 bits of address
 # Correlating predictor (local)  (5,1) (5 bits of address every address pattern has its own shift register
+# Gshare predictor with 10 bits address and 10 bit shift registers
 
 
 class SaturatedCounter:
@@ -58,8 +59,8 @@ class nBitPredictor:
         self.address_dict[address].dec()
 
 
-def hex_to_last_5(hex_string):
-    return bin(int(hex_string, 16))[2:].zfill(5)[-5:]
+def hex_to_last_n(hex_string, n=5):
+    return bin(int(hex_string, 16))[2:].zfill(n)[-n:]
 
 
 def run(debug=1):
@@ -82,7 +83,8 @@ def run(debug=1):
     output_columns = ["file name",
                       "total branches",
                       "global mispredictions", "global misprediction rate",
-                      "local mispredictions", "local misprediction rate"]
+                      "local mispredictions", "local misprediction rate",
+                      "gshare mispredictions", "gshare misprediction rate"]
     output_array = pd.DataFrame(columns=output_columns)
 
     for file_name in file_names:
@@ -91,16 +93,26 @@ def run(debug=1):
         global_5_1 = nBitPredictor(n=5)
         global_history = 'NNNNN'
         local_5_1 = nBitPredictor(n=5)
+        gshare = nBitPredictor(n=10)
+        gshare_history = '0000000000'
 
         for index, row in df.iterrows():
+            if debug != 0:
+                print("gshare_history:\t" + gshare_history)
+                print("address:\t\t" + hex_to_last_n(row['instruction_address'], n=10))
+                print("XOR:\t\t\t" + str(int(gshare_history, 2) ^ int(hex_to_last_n(row['instruction_address'], n=10), 2)))
             if row['taken_or_not'] == 'T':
                 global_5_1.taken(address=global_history)
                 global_history = global_history[1:] + 'T'
-                local_5_1.taken(hex_to_last_5(row['instruction_address']))
+                local_5_1.taken(hex_to_last_n(row['instruction_address'], n=5))
+                gshare.taken(str(int(gshare_history, 2) ^ int(hex_to_last_n(row['instruction_address'], n=10), 2)))
+                gshare_history = gshare_history[1:] + '1'
             else:
                 global_5_1.not_taken(address=global_history)
                 global_history = global_history[1:] + 'N'
-                local_5_1.not_taken(hex_to_last_5(row['instruction_address']))
+                local_5_1.not_taken(hex_to_last_n(row['instruction_address'], n=5))
+                gshare.not_taken(str(int(gshare_history, 2) ^ int(hex_to_last_n(row['instruction_address'], n=10), 2)))
+                gshare_history = gshare_history[1:] + '0'
 
         print("\nglobal stats")
         print(global_5_1.incorrect / global_5_1.total)
@@ -110,9 +122,14 @@ def run(debug=1):
         print(local_5_1.incorrect / local_5_1.total)
         print(local_5_1.total)
 
+        print("\ngshare stats")
+        print(gshare.incorrect / gshare.total)
+        print(gshare.total)
+
         new_row = [file_name, global_5_1.total,
                    global_5_1.incorrect, global_5_1.incorrect / global_5_1.total,
-                   local_5_1.incorrect, local_5_1.incorrect / local_5_1.total]
+                   local_5_1.incorrect, local_5_1.incorrect / local_5_1.total,
+                   gshare.incorrect, gshare.incorrect / gshare.total]
         output_array.loc[len(output_array.index)] = new_row
 
     output_array = output_array.set_index("file name")
@@ -121,4 +138,4 @@ def run(debug=1):
     pd.DataFrame(output_array).to_csv("branch_prediction_summary_task_4.csv")
 
 
-run(debug=0)
+run(debug=1)
